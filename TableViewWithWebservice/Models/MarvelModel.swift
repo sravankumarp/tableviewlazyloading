7cfff8365ca9d6593c6c5bd2495a3c76c2e1786b
Sravan <sravankumar.pabolu@cognizant.com>
2019-03-21 11:13:27 +0530

    Base Project

---
+++ TableViewWithWebservice/Models/MarvelModel.swift
@@ -0,0 +1,18 @@
+//
+//  MarvelModel.swift
+//  TableViewWithWebservice
+//
+//  Created by Sravan on 21/03/19.
+//  Copyright © 2019 Sravan. All rights reserved.
+//
+
+import Foundation
+
+struct MarvelModel {
+    let name: String
+    let realName: String
+    let team: String, firstAppearance: Int64, createdBy: String
+    let publisher: String
+    let imageURL: String
+    let bio: String
+}
---
+++ TableViewWithWebservice/Utils/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  TableViewWithWebservice
+//
+//  Created by Sravan on 21/03/19.
+//  Copyright © 2019 Sravan. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+//    let name = "Name"
+}
