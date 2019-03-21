41362e9daf3f0e2fa590591ab17002caed44258d
Sravan <sravankumar.pabolu@cognizant.com>
2019-03-21 11:03:58 +0530

    Initial Commit

---
+++ TableViewWithWebservice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewWithWebservice
+//
+//  Created by Sravan on 21/03/19.
+//  Copyright © 2019 Sravan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
